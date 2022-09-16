40e6f23edd5ccd7dc59a1799364a065148337e1c
Kevin Thompson <kevin@theautomaters.com>
2022-09-15 19:16:58 -0700

    Initial commit of lexer for Panther

---
+++ build.cmd
@@ -0,0 +1,10 @@
+@echo off
+@REM SET PNC=dotnet pnc
+SET PNC="D:\code\Panther2\src\Panther\bin\Debug\net6.0\pnc.exe"
+mkdir bin 2>NUL
+copy support\dotnet\* bin\ >NUL
+del bin\panther.dll 2>NUL
+%PNC% src\compiler\syntax.pn src\compiler\scanner.pn src\compiler\ast.pn src\compiler\parser.pn src\compiler\program.pn /o bin\panther.dll /r ".\support\dotnet\Panther.StdLib.dll" /r ".\support\dotnet\System.Runtime.dll" /r ".\support\dotnet\System.Runtime.Extensions.dll"
+if not exist .\bin\panther.dll exit /b
+cls
+dotnet .\bin\panther.dll src\compiler\syntax.pn src\compiler\scanner.pn src\compiler\ast.pn src\compiler\parser.pn src\compiler\program.pn
