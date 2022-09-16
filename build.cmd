0078d95f161a2391896a06829953b4b014974724
Kevin Thompson <kevin@theautomaters.com>
2022-09-15 19:17:06 -0700

    Add binder and symbol printer

--- build.cmd
+++ build.cmd
@@ -1,10 +1,11 @@
 @echo off
 @REM SET PNC=dotnet pnc
 SET PNC="D:\code\Panther2\src\Panther\bin\Debug\net6.0\pnc.exe"
+SET SOURCES=src\compiler\math.pn src\compiler\hex.pn src\compiler\text.pn src\compiler\syntax.pn src\compiler\scanner.pn src\compiler\ast.pn src\compiler\parser.pn src\compiler\symbols.pn src\compiler\binder.pn src\compiler\printer.pn src\compiler\program.pn
 mkdir bin 2>NUL
 copy support\dotnet\* bin\ >NUL
 del .\bin\panther.dll 2>NUL
-%PNC% src\compiler\math.pn src\compiler\hex.pn src\compiler\text.pn src\compiler\syntax.pn src\compiler\scanner.pn src\compiler\ast.pn src\compiler\parser.pn src\compiler\printer.pn src\compiler\program.pn /o bin\panther.dll /r ".\support\dotnet\Panther.StdLib.dll" /r ".\support\dotnet\System.Runtime.dll" /r ".\support\dotnet\System.Runtime.Extensions.dll"
+%PNC% %SOURCES% /o bin\panther.dll /r ".\support\dotnet\Panther.StdLib.dll" /r ".\support\dotnet\System.Runtime.dll" /r ".\support\dotnet\System.Runtime.Extensions.dll"
 if not exist .\bin\panther.dll exit /b
 cls
-dotnet .\bin\panther.dll src\compiler\hex.pn src\compiler\math.pn src\compiler\text.pn src\compiler\syntax.pn src\compiler\scanner.pn src\compiler\ast.pn src\compiler\parser.pn src\compiler\printer.pn src\compiler\program.pn
+dotnet .\bin\panther.dll %SOURCES%
